15d4ad7b5ea8afe52f42424a9db543843e628643
George Ivanov <georgei@accretivetg.com>
2014-04-19 18:33:19 -0700

    Homework 4 done

--- Lesson 04 Homework Files/assignments/shelf/gymnasium-carousel/gymnasium-carousel-1.0.js
+++ Lesson 04 Homework Files/assignments/shelf/gymnasium-carousel/gymnasium-carousel-1.0.js
@@ -1,37 +1,43 @@
 /*
 
-Don't forget to document your plugin! Include any assumptions that your plugin is making.
+ Don't forget to document your plugin! Include any assumptions that your plugin is making.
 
-*/
+ */
+
+$(document.head).append('<link rel="stylesheet" href="gymnasium-carousel/gymnasium-carousel-1.0.css"/>')
 
 // Don't forget to add your own stylesheet.
 
-$(document).ready(function() {
-	// Set up our carousel.
-	var $carousel = $('.carousel-wrapper');
-	$carousel.cycle({
-		fx: 'scrollHorz',
-		speed: 400,
-		height: $carousel.height(),
-		width: $carousel.width(),
-		fit: true
-	}).cycle('pause');
+$.fn.makeCarouselFor = function (selectorPrevButton, selectorNextButton, selectorItem) {
+    // Set up our carousel.
+    var $carousel = this;
+    $carousel.cycle({
+        fx: 'scrollHorz',
+        speed: 400,
+        height: $carousel.height(),
+        width: $carousel.width(),
+        fit: true
+    }).cycle('pause');
 
-	// Set up the carousel buttons.
-	$('.nav-arrow-left').click(function() {
-		$carousel.cycle('prev');
-	});
-	$('.nav-arrow-right').click(function() {
-		$carousel.cycle('next');
-	});
+    // Set up the carousel buttons.
+    $(selectorPrevButton).click(function () {
+        $carousel.cycle('prev');
+    });
+    $(selectorNextButton).click(function () {
+        $carousel.cycle('next');
+    });
 
-	// Animate in.
-	$('ul.shelf > li').animate({ top: 0, left: 0 }, 700, 'easeOutBounce');
+    // Animate in.
+    $(selectorItem).animate({ top: 0, left: 0 }, 700, 'easeOutBounce');
 
-	// Set up hover effect.
-	$('ul.shelf > li').hover(
-		function() { $(this).children('.meta').stop().animate({ bottom: 0 }, 250) },
-		function() { $(this).children('.meta').stop().animate({ bottom: -22 }, 100) }
-	)
+    // Set up hover effect.
+    $(selectorItem).hover(
+        function () {
+            $(this).children('.meta').stop().animate({ bottom: 0 }, 250)
+        },
+        function () {
+            $(this).children('.meta').stop().animate({ bottom: -22 }, 100)
+        }
+    )
 
-});
+};
